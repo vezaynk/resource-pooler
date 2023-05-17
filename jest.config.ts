f25b54316bb2a030900e9e1032e71d33e78b3360
Slava Knyazev <slava@knyazev.io>
2023-05-17 17:20:16 +0000

    Require full test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,16 @@
-module.exports = {
+import type { Config } from 'jest';
+
+export default {
   preset: 'ts-jest',
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
-};
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
+  coverageThreshold: {
+    global: {
+      lines: 100,
+    },
+  },
+} satisfies Config;
